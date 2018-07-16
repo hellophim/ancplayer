9479069f7f555537842c46904eea68abca325c4c
AnhNC-ThinkPad <anhnc.com@gmail.com>
2018-07-16 07:17:03 +0700

    Update map source to ancdata

--- index.js
+++ index.js
@@ -19,8 +19,12 @@ ANCMedia.prototype = {
         // const setting
         "VIDEO_SELECTOR": '#ancVideo',
         "EPISODE_SELECTOR": '#ancEpisode',
+        "LIST_SERVER": 'youtube.com,rapidvideo.com',
+        "LIST_SERVER_NAME": 'Tube, Rapid'
 
     },
+
+    ancdata: [],
 
     request: function (url, options) {
         var options = options || {
@@ -59,7 +63,30 @@ ANCMedia.prototype = {
             source = content && content.match(/\[id\](.*?)\[\/id\]/);
             source = source && source[1];
         }
-        return source;
+        return source && source.split('|');
+    },
+
+    mapSource: function () {
+        var list_server = this.default && this.default.LIST_SERVER && this.default.LIST_SERVER.split(',');
+        var list_server_name = this.default && this.default.LIST_SERVER_NAME && this.default.LIST_SERVER_NAME.split(',');
+
+        // define ancdata
+        list_server.forEach(function (serve, index) {
+            this.ancdata.push({
+                server: serve,
+                name: list_server_name[index],
+                data: []
+            });
+        }, this);
+
+        // push data to ancdata
+        if (this.source && Array.isArray(this.source)) {
+            this.source.forEach(function (episode, index) {
+                console.log(episode);
+            });
+        } else {
+            new Error("Don't have data episode in HTML");
+        }
     },
 
     deploy: (function () {
@@ -67,6 +94,7 @@ ANCMedia.prototype = {
         this.template.url = this.source;
         videoArea.innerHTML = this.template.video;
 
+        this.mapSource.bind(this)();
     })
 
 
